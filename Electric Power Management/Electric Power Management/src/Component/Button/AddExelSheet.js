4c5018b3cc5490ca618e1e513bcc4ce1459ce594
AsithaIndrajith <asithaindrajithk9@gmail.com>
2019-04-07 12:15:35 +0530

    add changes

---
+++ Electric Power Management/Electric Power Management/src/Component/Button/AddExelSheet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const addExelSheet = () => {
+    return (
+        <div className="container-fluid">
+            <input className="btn btn-primary" type="file" style={{"padding":"10px",width: "100%"}} />
+        </div>
+
+    )
+}
+
+export default addExelSheet
---
+++ Electric Power Management/Electric Power Management/src/Component/Button/AddFaults.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const addFaults = () => {
+    return (
+        <div className="container-fluid">
+            <input className="btn btn-info" type="file" style={{"padding":"10px",width: "100%"}} />
+        </div>
+
+    )
+}
+
+export default addFaults
