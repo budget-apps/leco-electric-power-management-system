92665eeee644e0280eed5fedbe5e84f9df6773ac
AsithaIndrajith <asithaindrajithk9@gmail.com>
2019-04-16 19:43:33 +0530

    changed uis

--- Electric Power Management/Electric Power Management/src/Component/Dashboard/Dashboard.js
+++ Electric Power Management/Electric Power Management/src/Component/Dashboard/Dashboard.js
@@ -7,7 +7,7 @@ import AddExelSheet from '../Button/AddExelSheet'
 import AddFaults from '../Button/AddFaults'
 import SelectMap from '../Button/SelectMap'
 import FaultEdge from '../FaultEdge/FaultEdge'
-
+import Graph from '../Graph/Graph'
 import './Dashboard.css'
 
 
@@ -15,6 +15,36 @@ import './Dashboard.css'
 class Dashboard extends Component {
     state = {
         data: null,
+        graph: null,
+    }
+
+    createMap(){
+        const primary1=new Node(345,"Primary",900, 900);
+        const primary2=new Node(346,"Primary",500, 500);
+        const switch1 = new Node(247,"Switch",200,200,"Closed");
+        const switch2 = new Node(248,"Switch",200,200,"Closed");
+        const switch3 = new Node(249,"Switch",200,200,"Closed");
+        const switch4 = new Node(250,"Switch",150,200,"Closed");
+        const start=new Node(0,"Start",0,0)
+        const end=new Node(251,"End",0,0)
+
+        start.setAdjacent(primary1,0)
+        start.setAdjacent(primary2,0)
+        primary1.setAdjacent(switch1,40);
+        switch1.setAdjacent(switch2,40);
+        switch2.setAdjacent(switch3,40);
+        switch3.setAdjacent(end,40);
+        switch2.setAdjacent(switch4,20)
+
+        switch2.setFaultCurrent(true);
+        switch3.setFaultCurrent(false);
+        switch3.setCurrentPower(0);
+
+        const graph = new Graph(start)
+        graph.addVertertices([primary1,primary2,switch1,switch2,switch3,end])
+        this.setState({
+            graph: graph
+        })
     }
     render() {
         return (
@@ -44,7 +74,7 @@ class Dashboard extends Component {
                             <Map data={this.state.data}/>
                         </div>
                         <div className="col-md-3">
-                            <FaultEdge changed={this.faultSwitchInputHandler}/>
+                            <FaultEdge changed={this.faultSwitchInputHandler} graph={this.state.graph}/>
                             <Path/>
                         </div>
                     </div>
--- Electric Power Management/Electric Power Management/src/Component/Map/Map.js
+++ Electric Power Management/Electric Power Management/src/Component/Map/Map.js
@@ -15,7 +15,7 @@ class map extends Component{
             { key: 4, text: "247\nSwitch\nClosed","loc": "-300 -100"},
             { key: 5, text: "248\nSwitch\nClosed","loc": "-100 -100"},
             { key: 3, text: "346\nPrimary","loc": "-500 100"},
-            { key: 6, text: "249\nSwitch\nClosed","loc": "-100 -100"},
+            { key: 6, text: "249\nSwitch\nClosed","loc": "100 -100"},
             { key: 7, text: "250\nSwitch\nClosed","loc": "-300 100"},
             { key: 8, text: "End","loc": "200 0"},
         ],
@@ -35,30 +35,7 @@ class map extends Component{
     getData(){
     }
 
-    createMap(){
-        const primary1=new Node(345,"Primary",900, 900);
-        const primary2=new Node(346,"Primary",500, 500);
-        const switch1 = new Node(247,"Switch",200,200,"Closed");
-        const switch2 = new Node(248,"Switch",200,200,"Closed");
-        const switch3 = new Node(249,"Switch",200,200,"Closed");
-        const switch4 = new Node(250,"Switch",150,200,"Closed");
-        const start=new Node(0,"Start",0,0)
-        const end=new Node(251,"End",0,0)
 
-        start.setAdjacent(primary1,0)
-        start.setAdjacent(primary2,0)
-        primary1.setAdjacent(switch1,40);
-        switch1.setAdjacent(switch2,40);
-        switch2.setAdjacent(switch3,40);
-        switch3.setAdjacent(end,40);
-        switch2.setAdjacent(switch4,20)
-
-        switch2.setFaultCurrent(true);
-        switch3.setFaultCurrent(false);
-        switch3.setCurrentPower(0);
-
-        const graph = new Graph(start)
-    }
 
 
     //graph.addVertertices([primary1,primary2,switch1,switch2,switch3,end])
