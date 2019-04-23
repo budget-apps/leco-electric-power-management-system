53441198bf88ffff3e067ee6ae30194b65623b45
LahiruDulanjaya <lahiru@localhost.localdomain>
2019-04-23 17:31:53 +0530

    realtime database

---
+++ Electric Power Management/Electric Power Management/src/Component/Alert/alert.js
@@ -0,0 +1,11 @@
+
+import Swal from 'sweetalert2'
+
+const firebase =require('firebase')
+
+
+firebase.database().ref().child('electricMap').on('child_changed',snapshot=>{
+    Swal.fire('Database changed')
+
+    console.log(snapshot.val())
+  })
